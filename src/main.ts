10bc9ad212f75271a2d06cb71916e08764344556
mrdiv <mr.divyanshu10@gmail.com>
2024-07-15 02:00:59 +0530

    added controler and provides

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
       .setVersion('1.0')
       .addBearerAuth()
       .build();
+
+      
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('/swagger', app, document, {
